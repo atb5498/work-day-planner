f6d69fe38e8c42fb74f19f4e4fd35a486f325ba5
abickford <atb5498@gmail.com>
2019-12-20 15:11:25 -0600

    created onClick events for every input that allows input to remain on page after refresh

--- script.js
+++ script.js
@@ -2,22 +2,29 @@ $(document).ready(function () {
     var input1 = localStorage.getItem("input1");
     $("#input1").val(JSON.parse(input1));
 
-    var input2 = $("#input2");
-    var saveBtn2 = $(".btn2");
-    var input3 = $("#input3");
-    var saveBtn3 = $(".btn3");
-    var input4 = $("#input4");
-    var saveBtn4 = $(".btn4");
-    var input5 = $("#input5");
-    var saveBtn5 = $(".btn5");
-    var input6 = $("#input6");
-    var saveBtn6 = $(".btn6");
-    var input7 = $("#input7");
-    var saveBtn7 = $(".btn7");
-    var input8 = $("#input8");
-    var saveBtn8 = $(".btn8");
-    var input9 = $("#input9");
-    var saveBtn9 = $(".btn9");
+    var input2 = localStorage.getItem("input2");
+    $("#input2").val(JSON.parse(input2));
+
+    var input3 = localStorage.getItem("input3");
+    $("#input3").val(JSON.parse(input3));
+
+    var input4 = localStorage.getItem("input4");
+    $("#input4").val(JSON.parse(input4));
+
+    var input5 = localStorage.getItem("input5");
+    $("#input5").val(JSON.parse(input5));
+
+    var input6 = localStorage.getItem("input6");
+    $("#input6").val(JSON.parse(input6));
+
+    var input7 = localStorage.getItem("input7");
+    $("#input7").val(JSON.parse(input7));
+
+    var input8 = localStorage.getItem("input8");
+    $("#input8").val(JSON.parse(input8));
+
+    var input9 = localStorage.getItem("input9");
+    $("#input9").val(JSON.parse(input9));
 
     function updateTime() {
         var time = moment().format('MMMM Do YYYY, h:mm:ss a');
@@ -25,62 +32,65 @@ $(document).ready(function () {
         setInterval(updateTime, 1000);
     }
 
+    // var currentTime = moment().format();
+    // var timeNine = ('09:00 a');
+
+    // if (timeNine < currentTime. {
+    //     $("#inputNine").css("background-color", "red")
+    // }
+
     $(".btn1").on("click", function (event) {
         event.preventDefault();
         var input1 = $("#input1").val();
         localStorage.setItem("input1", JSON.stringify(input1));
     });
 
-    var inputNine = localStorage.getItem("inputNine");
-    $("#inputNine").val(JSON.parse(inputNine));
-
-
-    saveBtn2.on("click", function () {
+    $(".btn2").on("click", function (event) {
         event.preventDefault();
-        var inputValue2 = input2.val();
-        localStorage.setItem("inputValue2", JSON.stringify(inputValue2));
+        var input2 = $("#input2").val();
+        localStorage.setItem("input2", JSON.stringify(input2));
     });
 
-    saveBtn3.on("click", function () {
+    $(".btn3").on("click", function () {
         event.preventDefault();
-        var inputValue3 = input3.val();
-        localStorage.setItem("inputValue3", JSON.stringify(inputValue3));
+        var input3 = $("#input3").val();
+        localStorage.setItem("input3", JSON.stringify(input3));
     });
 
-    saveBtn4.on("click", function () {
+    $(".btn4").on("click", function () {
         event.preventDefault();
-        var inputValue4 = input4.val();
-        localStorage.setItem("inputValue4", JSON.stringify(inputValue4));
+        var input4 = $("#input4").val();
+        localStorage.setItem("input4", JSON.stringify(input4));
     });
 
-    saveBtn5.on("click", function () {
+    $(".btn5").on("click", function () {
         event.preventDefault();
-        var inputValue5 = input5.val();
-        localStorage.setItem("inputValue5", JSON.stringify(inputValue5));
+        var input5 = $("#input5").val();
+        localStorage.setItem("input5", JSON.stringify(input5));
     });
 
-    saveBtn6.on("click", function () {
+    $(".btn6").on("click", function () {
         event.preventDefault();
-        var inputValue6 = input6.val();
-        localStorage.setItem("inputValue6", JSON.stringify(inputValue6));
+        var input6 = $("#input6").val();
+        localStorage.setItem("input6", JSON.stringify(input6));
     });
 
-    saveBtn7.on("click", function () {
+    $(".btn7").on("click", function () {
         event.preventDefault();
-        var inputValue7 = input7.val();
-        localStorage.setItem("inputValue7", JSON.stringify(inputValue7));
+        var input7 = $("#input7").val();
+        localStorage.setItem("input7", JSON.stringify(input7));
     });
 
-    saveBtn8.on("click", function () {
+    $(".btn8").on("click", function () {
         event.preventDefault();
-        var inputValue8 = input8.val();
-        localStorage.setItem("inputValue8", JSON.stringify(inputValue8));
+        var input8 = $("#input8").val();
+        localStorage.setItem("input8", JSON.stringify(input8));
     });
 
-    saveBtn9.on("click", function () {
+    $(".btn9").on("click", function () {
         event.preventDefault();
-        var inputValue9 = input9.val();
-        localStorage.setItem("inputValue9", JSON.stringify(inputValue9));
+        var input9 = $("#input9").val();
+        localStorage.setItem("input9", JSON.stringify(input9));
     });
 
     updateTime();
